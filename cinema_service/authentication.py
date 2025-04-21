fa2f7e867ea714b4b27ab301bed4bf42cb12044b
Windings-Lab <janton20001@gmail.com>
2025-04-21 18:43:13 +0300

    feat(authentication): add newline for code readability

--- cinema_service/authentication.py
+++ cinema_service/authentication.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import get_user_model
 from rest_framework import authentication
 
+
 class AlwaysAuth(authentication.BaseAuthentication):
     def authenticate(self, request):
         user, _ = get_user_model().objects.get_or_create(username="admin.user")
